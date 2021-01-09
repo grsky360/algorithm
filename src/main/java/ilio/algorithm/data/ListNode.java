a60e082cf154ceddd222ed3dd5b8105e725be813
chenscl <chenscl@ziroom.com>
2021-01-09 12:43:15 +0800

    update

--- src/main/java/ilio/algorithm/data/ListNode.java
+++ src/main/java/ilio/algorithm/data/ListNode.java
@@ -5,11 +5,10 @@ public class ListNode {
     public ListNode next;
 
     public ListNode() {
-        this(0, null);
     }
 
     public ListNode(int val) {
-        this(val, null);
+        this.val = val;
     }
 
     public ListNode(int val, ListNode next) {
@@ -17,8 +16,20 @@ public class ListNode {
         this.next = next;
     }
 
+    public ListNode(int val, int... nexts) {
+        this.val = val;
+        ListNode curr = this;
+        for (int next : nexts) {
+            curr = curr.next = new ListNode(next);
+        }
+    }
+
     @Override
     public String toString() {
         return val + " -> " + next;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ListNode(1, 2, 3, 4, 5));
     }
 }
